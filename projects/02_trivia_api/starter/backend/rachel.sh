af13abc10ba2a761e961d22f52292d2cca6b2a6a
Rachel <rzhangmoz1@gmail.com>
2020-04-12 14:42:44 -0700

    add helper script

--- projects/02_trivia_api/starter/backend/rachel.sh
+++ projects/02_trivia_api/starter/backend/rachel.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -x
 
 cd /Users/rachel/full_stack_class/projects/fyyur/FSND/projects/02_trivia_api/starter/backend
-dropdb trivia
-createdb trivia
-psql -U Rachel -d trivia -a -f trivia.psql
+# dropdb trivia
+# createdb trivia
+dropdb trivia_test
+createdb trivia_test
+psql -U Rachel -d trivia_test -a -f trivia.psql
 rm -rf migrations
 flask db init
 flask db stamp head
 flask db migrate # from flask_migrate import Migrate, migrate = Migrate(app, db) 
 flask db upgrade
-#python3 ./rachel.py
 psql trivia -c '\dt'
 psql trivia -c 'select * from questions'
 psql trivia -c 'select * from categories'
