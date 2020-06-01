e379cfed7bb31aa9b8a7f5290b4f58145cacccc0
Rachel <rzhangmoz1@gmail.com>
2020-05-31 23:27:53 -0700

    added GET, PATCH and DELETE for dish items

--- projects/capstone/05_what_to_eat/backend/src/app.py
+++ projects/capstone/05_what_to_eat/backend/src/app.py
@@ -29,6 +29,35 @@ def create_app():
             'Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
         response.headers.add('Access-Control-Allow-Credentials', 'true')
         return response
+    
+    default_dish_image_link = "https://unsplash.com/photos/1Rm9GLHV0UA"
+    default_restaurant_image_link = "https://unsplash.com/photos/26T6EAsQCiA"
+    
+    def get_formatted_dish(dish_id):
+        formatted_dish = {}
+        try:
+            dish = Dish.query.get(dish_id)
+            if dish is None:
+                abort(404)
+        except Exception as e:
+            print(e)
+            abort(404)
+
+        dish_category = Category.query.get(dish.category_id)
+        dish_restaurant = Restaurant.query.get(dish.restaurant_id)
+
+        formatted_dish = {
+            'id': dish.id,
+            'name': dish.name,
+            'restaurant_id': dish.restaurant_id,
+            'category_id': dish.category_id,
+            'rating': dish.rating,
+            'price': float(dish.price),
+            'image_link': dish.image_link or default_dish_image_link,
+            'restaurant_name': dish_restaurant.name,
+            'category': dish_category.category
+        }
+        return formatted_dish
 
     @app.route('/')
     def hello_world():
@@ -70,6 +99,95 @@ def create_app():
             "dishes": formatted_all_dishes
         })
     
+    @app.route('/dishes/<int:dish_id>', methods=['GET'])
+    def get_dish_item(dish_id):        
+        formatted_dish = get_formatted_dish(dish_id)
+        # formatted_dish = {}
+        # try:
+        #     dish = Dish.query.get(dish_id)
+        #     if dish is None:
+        #         abort(404)
+        # except Exception as e:
+        #     print(e)
+        #     abort(404)
+
+        # dish_category = Category.query.get(dish.category_id)
+        # dish_restaurant = Restaurant.query.get(dish.restaurant_id)
+
+        # formatted_dish = {
+        #     'id': dish.id,
+        #     'name': dish.name,
+        #     'restaurant_id': dish.restaurant_id,
+        #     'category_id': dish.category_id,
+        #     'rating': dish.rating,
+        #     'price': float(dish.price),
+        #     'image_link': dish.image_link or default_dish_image_link,
+        #     'restaurant_name': dish_restaurant.name,
+        #     'category': dish_category.category
+        # }
+        return jsonify({
+                "success": True,
+                "dish": formatted_dish
+            })
+
+    @app.route('/dishes/<int:dish_id>', methods=['PATCH'])
+    def update_dish(dish_id):
+        body = request.get_json()
+        new_name = body.get('name')
+        new_restaurant_id = body.get('restaurant_id')
+        new_category_id = body.get('category_id')
+        new_rating = body.get('rating')
+        new_price = body.get('price')
+        new_image_link = body.get('image_link')
+
+        dish = Dish.query.get(dish_id)
+
+        if dish is None:
+            abort(404)
+        if new_name is not None:
+            dish.name = new_name
+        if new_restaurant_id is not None:
+            dish.restaurant_id = new_restaurant_id
+        if new_category_id is not None:
+            dish.category_id = new_category_id
+        if new_rating is not None:
+            dish.rating = new_rating
+        if new_price is not None:
+            dish.price = new_price
+        if new_image_link is not None:
+            dish.image_link = new_image_link
+
+        dish.update()
+
+        new_dish = get_formatted_dish(dish_id)
+
+        return jsonify({
+                "success": True,
+                "dish": new_dish
+            })
+
+    @app.route('/dishes/<int:dish_id>', methods=['DELETE'])
+    def delete_dish(dish_id):
+        try:
+            delete_dish_id = dish_id
+            dish = Dish.query.get(dish_id)
+            #print(dish)
+        # except Exception as e:
+        #     print(e)
+        #     abort(404)
+            print(dish)
+            if dish is None:
+                abort(404)
+            dish.delete()
+            return jsonify({
+                    "success": True,
+                    "deleted dish": delete_dish_id
+                })
+        except IndexError:
+             abort(404)
+
+
+
     @app.route('/categories', methods=['GET'])
     def get_categories():
         try:
