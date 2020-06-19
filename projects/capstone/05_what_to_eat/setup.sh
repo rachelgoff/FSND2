4ef870e6bca12cf852bdb9cc3072c96b9f4ee758
Rachel <rzhangmoz1@gmail.com>
2020-06-19 12:58:19 -0700

    update setup.sh

--- projects/capstone/05_what_to_eat/setup.sh
+++ projects/capstone/05_what_to_eat/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -x
 
 # create database with name as dish
-# dropdb dish
-createdb dish
+# DATABASE_NAME='dish'
+# if psql -lqt | cut -d \| -f 1 | grep -qw $DATABASE_NAME; then
+#     echo "A database with the name $DBNAME already exists."
+#     echo "Drop $DATABASE_NAME and create a new one."
+#     dropdb $DATABASE_NAME
+# fi
+createdb $DATABASE_NAME
 
 # Export the environment variables and set up for the server
 export DATABASE_URL='postgresql://localhost:5432/dish' 
